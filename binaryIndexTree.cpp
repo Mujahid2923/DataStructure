1b9a47592a5063905e12763e60b7df2f939f6d6f
Mujahidul Islam <32108024+Mujahid2923@users.noreply.github.com>
2019-09-25 15:48:22 +0600

    Update binaryIndexTree.cpp

--- binaryIndexTree.cpp
+++ binaryIndexTree.cpp
@@ -1,48 +1,114 @@
-#include<bits/stdc++.h>
-using namespace std;
+///...................................*****.................................................///
+///                  Mujahidul Islam ( mujahidulislam2923@gmail.com )                       ///
+///                  Department of Ict                                                      ///
+///                  Comilla University , Bangladesh.                                       ///
+///...................................*****.................................................///
 
-int query(int tree[],int idx)
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template<typename T> using orderset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+
+#define          CIN                    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define          ll                     long long int
+#define          ull                    unsigned long long int
+#define          pii                    pair < int, int>
+#define          pll                    pair < ll, ll>
+#define          MOD                    1000000007
+#define          vi                     vector<int>
+#define          vl                     vector<ll>
+#define          pb                     push_back
+#define          sc                     scanf
+#define          pf                     printf
+#define          scin(x)                scanf("%d",&(x))
+#define          scin2(x,y)             scanf("%d %d",&(x),&(y))
+#define          scln(x)                scanf("%lld",&(x))
+#define          scln2(x,y)             scanf("%lld %lld",&(x),&(y))
+#define          min3(a,b,c)            min(a,min(b,c))
+#define          min4(a,b,c,d)          min(d,min(a,min(b,c)))
+#define          max3(a,b,c)            max(a,max(b,c))
+#define          max4(a,b,c,d)          max(d,max(a,max(b,c)))
+#define          ms(a,b)                memset(a,b,sizeof(a))
+#define          mp                     make_pair
+#define          gcd(a, b)              __gcd(a,b)
+#define          lcm(a, b)              ((a)*(b)/gcd(a,b))
+#define          input                  freopen("input.txt","rt", stdin)
+#define          output                 freopen("output.txt","wt", stdout)
+#define          PI                     3.141592653589793238462643
+#define          zero(a)                memset(a,0,sizeof a)
+#define          all(v)                 v.begin(),v.end()
+#define          Max(v)                 *max_element(all(v))
+#define          Upper(c,x)             (upper_bound(c.begin(),c.end(),x)-c.begin())
+#define          Lower(c,x)             (lower_bound(c.begin(),c.end(),x)-c.begin())
+#define          Unique(X)              (X).erase(unique(all(X)),(X).end())
+
+///priority_queue<int,vector<int>,greater<int> >pq;
+///string str = "abcdefghijklmnopqrstuvwxyz";
+///string s2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
+
+///---------------Order set-------------------
+///template<typename T> using orderset =tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+///#include <ext/pb_ds/assoc_container.hpp>
+///using namespace __gnu_pbds;
+///*os.find_by_order(index) os.order_of_key(value)
+
+///--------------Graph Moves--------------------------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+///---------------------------------------------------------------
+///sort( all( v ) ) ;
+
+ll arr[ 100005 ] ;
+ll trees[ 100005 ] ;
+ll n , d , idx , val ;
+
+ll SumQuery( ll idx )
 {
-    idx+=1;
-    int sum=0;
-    while(idx>0)
+    ll sum = 0 ;
+    while( idx > 0 )
     {
-        sum += tree[idx];
-        idx -= (idx & (-idx));
+        sum += trees[ idx ] ;
+        idx -= ( idx & ( -idx ) ) ;
     }
     return sum;
 }
 
-void update(int tree[],int n,int idx,int x)
+void update( ll idx , ll x )
 {
-    idx+=1;
-    while(idx<=n)
+    while( idx <= n )
     {
-        tree[idx] += x;
-        idx += (idx & (-idx));
+        trees[ idx ] += x ;
+        idx += ( idx & ( -idx ) ) ;
     }
 }
 
 int main()
 {
-    int n,d;
-    cin>>n;
-    int arr[n+1];
-    for(int i=0; i<=n; i++)
+    CIN ;
+    cin >> n ;
+    for( int i = 1 ; i <= n ; i ++ )
     {
-        cin>>arr[i];
+        cin >> arr[ i ] ;
+        update( i , arr[ i ] ) ;
     }
-    ///Sum
-    cout<<"Till how you want to get the sum:"<<endl;
-    cin>>d;
-    int ans1=query(arr,d);
-    cout<<"Sum is :"<<ans1<<endl;
 
-    ///Update
-   // arr[10]+=20;
-    update(arr,n,10,20);//arr,index,number to add
-    int ans2=query(arr,d);
-    cout<<"Now the sum is :"<<ans2<<endl;
+    cout << "Till how you want to get the sum:" << endl ;
+    cin >> d ;
+    ll ans1 = SumQuery( d ) ;
+    cout << "Sum is :"<< ans1 << endl ;
+
+    cout << "Which index which value:" << endl ;
+    cin >> idx >> val ;
+    update( idx , val ) ;
+    int ans2 = SumQuery( d ) ;
+
+    cout<<"Now the sum is :"<< ans2 <<endl;
 }
 
 /*
@@ -57,5 +123,3 @@ sum is 12
 sum is 91
 sum is 111
 */
-
-

5fbe0d0dcf3c34ba7da2a411cb6c44775bf98d01
Mujahidul Islam <32108024+Mujahid2923@users.noreply.github.com>
2020-01-30 01:36:25 +0600

    Update segmentedTreeSum.cpp

--- segmentedTreeSum.cpp
+++ segmentedTreeSum.cpp
@@ -1,93 +1,162 @@
+///...................................*****.................................................///
+///                  Mujahidul Islam ( mujahidulislam2923@gmail.com )                       ///
+///                  Department of Ict                                                      ///
+///                  Comilla University , Bangladesh.                                       ///
+///...................................*****.................................................///
+
 #include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+template<typename T> using orderset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
 
-#define mx 1000
-int arr[mx];
-int tree[mx*3];
+#define          CIN                    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define          ll                     long long int
+#define          ld                     long double
+#define          ull                    unsigned long long int
+#define          pii                    pair < int, int>
+#define          pll                    pair < ll, ll>
+#define          MOD                    1000000007
+#define          vi                     vector<int>
+#define          vl                     vector<ll>
+#define          pb                     push_back
+#define          sc                     scanf
+#define          pf                     printf
+#define          scin(x)                scanf("%d",&(x))
+#define          scin2(x,y)             scanf("%d %d",&(x),&(y))
+#define          scln(x)                scanf("%lld",&(x))
+#define          scln2(x,y)             scanf("%lld %lld",&(x),&(y))
+#define          min3(a,b,c)            min(a,min(b,c))
+#define          min4(a,b,c,d)          min(d,min(a,min(b,c)))
+#define          max3(a,b,c)            max(a,max(b,c))
+#define          max4(a,b,c,d)          max(d,max(a,max(b,c)))
+#define          ms(a,b)                memset(a,b,sizeof(a))
+#define          mp                     make_pair
+#define          gcd(a, b)              __gcd(a,b)
+#define          lcm(a, b)              ((a)*(b)/gcd(a,b))
+#define          input                  freopen("input.txt","rt", stdin)
+#define          output                 freopen("output.txt","wt", stdout)
+#define          PI                     acos(-1.0)
+#define          zero(a)                memset(a,0,sizeof a)
+#define          all(v)                 v.begin(),v.end()
+#define          Max(v)                 *max_element(all(v))
+#define          Min(v)                 *min_element(all(v))
+#define          Upper(c,x)             (upper_bound(c.begin(),c.end(),x)-c.begin())
+#define          Lower(c,x)             (lower_bound(c.begin(),c.end(),x)-c.begin())
+#define          Unique(X)              (X).erase(unique(all(X)),(X).end())
+#define          no                     cout << "NO" << endl ;
+#define          yes                    cout << "YES" << endl ;
+///#define          endl                   '\n'
 
-int query(int node,int b,int e,int i,int j)
+///bool operator < ( const Node& p ) const{ return cost < p.cost ; }
+///priority_queue<int,vector<int>,greater<int> >pq;
+///string str = "abcdefghijklmnopqrstuvwxyz";
+///string s2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
+///string s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
+///string s = std::bitset<64>(n).to_string();
+///---------------Order set-------------------
+///template<typename T> using orderset =tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+///#include <ext/pb_ds/assoc_container.hpp>
+///using namespace __gnu_pbds;
+///*os.find_by_order(index) os.order_of_key(value)
+
+///--------------Graph Moves--------------------------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+///---------------------------------------------------------------
+
+#define mx 100005
+
+int arr[ mx ];
+int Tree[ mx * 4 ];
+
+int query( int node, int b, int e, int i, int j )
 {
-    if(b > j || e < i)
+    if( b > j || e < i )
     {
-        return 0;
+        return 0 ;
     }
 
-    if(b >= i && e <= j)
+    if( b >= i && e <= j )
     {
-        return tree[node];
+        return Tree[ node ] ;
     }
 
+    int Lnode = node * 2 ;
+    int Rnode = node * 2 + 1 ;
 
-    int Lnode=node*2;
-    int Rnode=node*2+1;
+    int mid = ( b + e ) / 2 ;
+    int s1 = query( Lnode, b, mid, i, j ) ;
+    int s2 = query( Rnode, mid + 1, e, i, j ) ;
 
-    int mid=(b+e)/2;
-    int s1=query(Lnode,b,mid,i,j);
-    int s2=query(Rnode,mid+1,e,i,j);
-    return s1+s2;
+    return s1 + s2 ;
 }
 
-void update(int node,int b,int e,int i,int newvalue)
+void update( int node, int b, int e, int i, int newvalue )
 {
-    if(i>e || i<b)
+    if( i > e || i < b )
     {
-        return;
-    }
-    if(b>=i && e<=i)
-    {
-        tree[node]=newvalue;
-        return;
+        return ;
     }
 
-    int Left=node*2;
-    int Right=node*2+1;
+    if( b >= i && e <= i )
+    {
+        Tree[ node ] = newvalue ;
+        return ;
+    }
 
-    int mid=(b+e)/2;
-    update(Left,b,mid,i,newvalue);
-    update(Right,mid+1,e,i,newvalue);
+    int Left = node * 2 ;
+    int Right = node * 2 + 1 ;
 
-    tree[node]=tree[Left]+tree[Right];
+    int mid = ( b + e ) / 2 ;
+    update( Left, b, mid, i, newvalue ) ;
+    update( Right, mid + 1, e, i, newvalue ) ;
+
+    Tree[ node ] = Tree[ Left ] + Tree[ Right ] ;
 }
 
 
-void init(int *arr,int node,int b,int e)
+void init( int node, int b, int e )
 {
-    if(b==e)
+    if( b == e )
     {
-        tree[node]=arr[b];
-        return;
+        Tree[ node ] = arr[ b ] ;
+        return ;
     }
 
-    int Left=node*2;
-    int Right=node*2+1;
+    int Left = node * 2 ;
+    int Right = node * 2 + 1 ;
+    int mid = ( b + e ) / 2 ;
 
-    int mid=(b+e)/2;
-
-    init(arr,Left,b,mid);
-    init(arr,Right,mid+1,e);
-    tree[node]=tree[Left]+tree[Right];
+    init( Left, b, mid ) ;
+    init( Right, mid + 1, e ) ;
+    Tree[ node ] = Tree[ Left ] + Tree[ Right ] ;
 }
 
 int main()
 {
     int n;
-    cin>>n;
-    int arr[n+1];
+    cin >> n ;
 
-    for(int i=1; i<=n; i++)
+    for( int i = 1 ; i <= n ; i ++ )
     {
-        cin>>arr[i];
+        cin >> arr[ i ] ;
     }
 
-    int a,b;
-    cin>>a>>b;
-    init(arr,1,1,n);///For transforming array into segment tree usable array
+    int a, b ;
+    cin >> a >> b ;
+    init( 1, 1, n ) ;  ///For transforming array into segment tree usable array
 
-    int ans1=query(1,1,n,a,b);///Where add will be occur
-    cout<<"Normal sum before update :"<<endl<<ans1<<endl;
-    update(1,1,n,8,-10);
-    int ans2=query(1,1,n,a,b);
-    cout<<"Sum after update :"<<endl<<ans2<<endl;
+    int ans1 = query( 1, 1, n, a, b ) ;    ///Where add will be occur
+    cout <<"Normal sum before update :"<<endl<<ans1<<endl;
+    update( 1, 1, n, 8, -10 ) ;
+    int ans2 = query( 1, 1, n, a, b ) ;
+    cout << "Sum after update :" << endl << ans2 << endl ;
     return 0;
 
 }
